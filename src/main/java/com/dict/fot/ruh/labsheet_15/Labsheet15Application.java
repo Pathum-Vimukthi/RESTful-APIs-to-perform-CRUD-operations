2450cbe9308fa2d9192d1424c9efebc0523bd687
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2024-04-22 22:48:16 +0530

    Initial RESTFull APIs Project

---
+++ src/main/java/com/dict/fot/ruh/labsheet_15/Labsheet15Application.java
@@ -0,0 +1,13 @@
+package com.dict.fot.ruh.labsheet_15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labsheet15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Labsheet15Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dict/fot/ruh/labsheet_15/Labsheet15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dict.fot.ruh.labsheet_15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labsheet15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
